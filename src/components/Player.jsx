9ec4bead848f2f04c1f26820b9e0ee4f86dfed0c
ritacc12 <rita20920@gmail.com>
2024-02-01 16:40:59 +0800

    import useRef to replace submitted state control (only need to read the value)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enterPlayerName, setEnterPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnterPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnterPlayerName(playerName.current.value);
+    //player name change to empty
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enterPlayerName ? enterPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
