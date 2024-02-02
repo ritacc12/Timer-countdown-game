d212ff4e1e881b9e6b4b734161c1b728d6be7d51
ritacc12 <rita20920@gmail.com>
2024-02-02 16:23:22 +0800

    import web API setTimeout and manage the timer state (start & expired)

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,39 @@
+import { useState, useRef } from "react";
+
 export default function TimeChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const [timeStarted, setTimeStarted] = useState(false);
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+
+    setTimeStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">{targetTime} second</p>
-      <p>
-        <button>Start</button>
-      </p>
-      <p className="">Time is running</p>
-    </section>
+    <>
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timeExpired && <p>You lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timeStarted ? handleStop : handleStart}>
+            {timeStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timeStarted ? "active" : undefined}>
+          {timeStarted ? "Time is running" : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
