aa2e6282eeb5eea38e10aa700bb0cf6ac11f4d2f
ritacc12 <rita20920@gmail.com>
2024-02-03 16:55:51 +0800

    import Hook useImperativehandle to customize the handle exposed as a ref

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,13 +1,23 @@
 //forwardRef lets your component expose a DOM node to parent component with a ref.
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime },
   ref
 ) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
     // built-in dialog by default is invisible , but it can be made visible by adding open prop to it
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds</strong>
